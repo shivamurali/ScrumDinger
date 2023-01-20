c5c376c394af4874f13571ca11a0db82d6c1a626
shivamurali <pandiham@gmail.com>
2023-01-20 17:50:48 -0500

    Initial Commit

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Shiva Murali on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
